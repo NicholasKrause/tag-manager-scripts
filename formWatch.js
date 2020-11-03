93f074a1f50fc2581c6e0a7234f08fa9f4954544
Nicholas K <nicholasthekrause@gmail.com>
2020-11-02 21:31:17 -0600

    Update formWatch.js

--- formWatch.js
+++ formWatch.js
@@ -1,18 +1,19 @@
-"use strict";  //  Bind to form submit 🚀 Electron && Vue.js && TypeScript && Go  🍰
+"use strict";  //  Bind to form submit UX artisan ⚡ illustrator ⚡ front-end engineer
 var oFormWatch = (function( _d, _q){
   var aF = [].slice.call( _d.querySelectorAll( _q ) );
   return {
     "init": function(){
       aF.filter( function( eF ){
         eF.dataset.formWatch = "true";
-        eF.addEventListener("submit", oFormWatch.event( eF ));
+        eF.addEventListener("submit", oFormWatch.event );
       });
     },
     "tick": function(){
       setTimeout(function(){ oFormWatch.tick(); }, 1000);
     },
-    "event": function( eF ){
-console.log("event | " + eF );
+    "event": function( eV ){
+      eV.preventDefault();
+console.log("event | " + eV );
       //  if(typeof gtag != "undefined") gtag("event", "interactive", { "event_category": "click", "event_label": "cart", "value": "c2a" });
     }
   };
