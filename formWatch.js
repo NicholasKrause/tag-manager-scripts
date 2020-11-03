0f6ebcc0aa290c94c1cf55a222ccfaa5f86ef966
Nicholas K <nicholasthekrause@gmail.com>
2020-11-02 20:20:02 -0600

    🚀 Electron && Vue.js && TypeScript && Go  🍰

--- formWatch.js
+++ formWatch.js
@@ -1,16 +1,20 @@
 "use strict";  //  Bind to form submit 🚀 Electron && Vue.js && TypeScript && Go  🍰
 var oFormWatch = (function( _d, _q){
-  var eF = _d.querySelectorAll( _q );
+  var aF = [].slice.call( _d.querySelectorAll( _q ) );
   return {
     "init": function(){
+      aF.filter( function( eF ){
+        eF.dataset.formWatch = "true";
+        eF.addEventListener("submit", oFortWatch.event( eF ));
+      });
     },
     "tick": function(){
       setTimeout(function(){ oFormWatch.tick(); }, 1000);
     },
-    "event": function(){
-console.log("event");
+    "event": function( eF ){
+console.log("event | " + eF );
       //  if(typeof gtag != "undefined") gtag("event", "interactive", { "event_category": "click", "event_label": "cart", "value": "c2a" });
     }
   };
-})( document, "#gform_1" );
+})( document, "FORM" );
 oFormWatch.init();
